ece9ab4c103960b33c24d40c7f148e7ed2a69adb
abdelmoniem ahmed <3bdelmoniemahmed11@gmail.com>
2020-08-29 01:34:44 +0200

    third commit

--- js/Home.js
+++ js/Home.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-$(".clients").not('.slick-initialized').slick({
+$(".clients").slick({
     dots: !1,
       infinite: !0,
       speed: 300,
@@ -17,6 +17,33 @@ $(".clients").not('.slick-initialized').slick({
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+})
+$('.home-Gallery').slick({
+  dots: !1,
+      infinite: !0,
+      speed: 300,
+      slidesToShow: 3,
+      touchMove: !0,
+      slidesToScroll: 1,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       }, {
         breakpoint: 480,
