98b156299469299c4ebc1765690a499bd6d7a5d3
abdelmoniem ahmed <3bdelmoniemahmed11@gmail.com>
2020-09-03 02:21:56 +0200

    fifth commit

--- js/Home.js
+++ js/Home.js
@@ -53,9 +53,33 @@ $('.home-Gallery').slick({
         }
       }]
 })
+$('.people').slick({
+  dots: !1,
+  infinite: !0,
+  speed: 300,
+  slidesToShow: 2,
+  touchMove: !0,
+  slidesToScroll: 1,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 640,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }]
 })
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip(
-    Animation
-  )
+
+
 })
