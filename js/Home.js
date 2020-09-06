484116330d957a9a1e2163b3459693d168ee3fcb
abdelmoniem ahmed <3bdelmoniemahmed11@gmail.com>
2020-09-07 00:47:27 +0200

    in-progress

--- js/Home.js
+++ js/Home.js
@@ -80,7 +80,9 @@ $('.people').slick({
     }
   }]
 })
+
 });
+
 
 const haschildern=document.querySelectorAll('.haschildern');
 const shortcodes_box=document.getElementById('shortcodes-box');
@@ -131,6 +133,24 @@ search_icon.addEventListener('click',()=>{
     search_box[0].style.visibility='hidden';
     search_box[0].style.opacity=0;
   }
-  
-})
 
+})
+$(window).scroll( function(){
+
+  "use strict";
+  /* Check the location of each desired element */
+  $('.fun-staff').each( function(i){
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > bottom_of_object ){
+
+          $(".count").countTo();
+
+      }
+
+  }); 
+
+});
